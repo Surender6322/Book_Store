8fa3207abf30e235cd1669c348b15a1466ff0df0
vikkiakavc <vschaudhary2001@gmail.com>
2024-02-06 12:29:04 +0530

    resolve error

--- src/controllers/books.js
+++ src/controllers/books.js
@@ -26,7 +26,7 @@ const addBook = async (req, res) => {
   }
 };
 
-//get books
+// get books
 const library = async (req, res) => {
   try {
     const {
@@ -43,101 +43,66 @@ const library = async (req, res) => {
 
     let filters = {};
 
-    // category as multiselect filter
+    // Apply filters based on query parameters
+
+    // Apply category filter
     if (categories) {
-      if (Array.isArray(categories) && categories.length > 0) {
-        filters.name = {
-          [Op.in]: categories,
-        };
-      } else {
-        filters.name = categories;
+      filters.name = {
+        [Op.in]: Array.isArray(categories) ? categories : [categories],
+      };
+    }
+
+    // Apply publication year filter
+    if (minPublicationYear || maxPublicationYear) {
+      filters.publicationYear = {};
+      if (minPublicationYear) {
+        filters.publicationYear[Op.gte] = minPublicationYear;
+      }
+      if (maxPublicationYear) {
+        filters.publicationYear[Op.lte] = maxPublicationYear;
       }
     }
 
-    // publication year range filter
-    if (minPublicationYear && maxPublicationYear) {
-      filters.publicationYear = {
-        [Op.between]: [minPublicationYear, maxPublicationYear],
-      };
-    } else if (minPublicationYear) {
-      filters.publicationYear = {
-        [Op.gte]: minPublicationYear,
-      };
-    } else if (maxPublicationYear) {
-      filters.publicationYear = {
-        [Op.lte]: maxPublicationYear,
-      };
+    // Apply cost filter
+    if (minCost || maxCost) {
+      filters.cost = {};
+      if (minCost) {
+        filters.cost[Op.gte] = minCost;
+      }
+      if (maxCost) {
+        filters.cost[Op.lte] = maxCost;
+      }
     }
 
-    // cost range filter
-    if (minCost && maxCost) {
-      filters.cost = {
-        [Op.between]: [minCost, maxCost],
-      };
-    } else if (minCost) {
-      filters.cost = {
-        [Op.gte]: minCost,
-      };
-    } else if (maxCost) {
-      filters.cost = {
-        [Op.lte]: maxCost,
-      };
-    }
-
-    // rating filter
+    // Apply rating filter
     if (minRating) {
       filters.rating = {
-        [Op.gte]: [minRating],
+        [Op.gte]: minRating,
       };
     }
 
-    // order date range filter
-    if (minOrderDate && maxOrderDate) {
-      filters.order_date = {
-        [Op.between]: [minOrderDate, maxOrderDate],
-      };
-    } else if (minOrderDate) {
-      filters.order_date = {
-        [Op.gte]: minOrderDate,
-      };
-    } else if (maxOrderDate) {
-      filters.order_date = {
-        [Op.lte]: maxOrderDate,
-      };
-    }
-
-    // languages as multiselect filter
-    if (languages) {
-      if (Array.isArray(languages) && languages.length > 0) {
-        filters.lang = {
-          [Op.in]: languages,
-        };
-      } else {
-        filters.lang = languages;
+    // Apply order date filter
+    if (minOrderDate || maxOrderDate) {
+      filters.order_date = {};
+      if (minOrderDate) {
+        filters.order_date[Op.gte] = minOrderDate;
+      }
+      if (maxOrderDate) {
+        filters.order_date[Op.lte] = maxOrderDate;
       }
     }
 
+    // Apply language filter
+    if (languages) {
+      filters.lang = {
+        [Op.in]: Array.isArray(languages) ? languages : [languages],
+      };
+    }
+
+    // Find books with applied filters
     const books = await Books.findAll({
       where: filters,
-      include: [
-        {
-          model: Languages,
-          where: filters,
-        },
-        {
-          model: Reviews,
-          where: filters,
-        },
-        {
-          model: Categories,
-          where: filters,
-        },
-        {
-          model: Orders,
-          where: filters,
-        },
-      ],
-      distinct: true,
+      include: [Languages, Reviews, Categories, Orders],
     });
 
     res.send({ books });
